2cc18d527125fff7b7d8b03ffef01987400399fe
Marcello Tascioni <33826382+mTascioni@users.noreply.github.com>
2025-06-25 10:47:31 +0200

    Update Attuazione_Centrale.ino

--- centralModule/Attuazione_Centrale/Attuazione_Centrale.ino
+++ centralModule/Attuazione_Centrale/Attuazione_Centrale.ino
@@ -94,7 +94,7 @@ void actuate_Windows(bool open_windows)
 
   if(open_windows && !windows_open)
   {
-    for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
       // in steps of 1 degree
       myservo1.write(pos);              
       myservo2.write(pos);             
@@ -106,7 +106,7 @@ void actuate_Windows(bool open_windows)
   else 
   if(!open_windows && windows_open)
   {
-    for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
       myservo1.write(pos);              // tell servo to go to position in variable 'pos'
       myservo2.write(pos);              // tell servo to go to position in variable 'pos'
       delay(15);                       // waits 15ms for the servo to reach the position
